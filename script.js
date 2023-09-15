2b49d76895ff97798937741eae984f291a7924c7
mdGoswami <u19cse1001@cit.ac.in>
2023-09-15 21:24:59 +0530

    feat: functionalities

--- script.js
+++ script.js
@@ -1,12 +1,70 @@
+// Variables used in multiple functions
 const moves = ["Rock", "Paper", "Scissors"];
-let computer = document.getElementById('computer-score');
-let player =document.getElementById('player-score');
+let computer = document.querySelector('.computer-score');
+let player = document.querySelector('.player-score');
+const nextButton = document.querySelector('.next');
+const resetButton = document.querySelector('.reset');
+const playerButtons = document.querySelector('.play-btns');
+const roundResult = document.querySelector('.round-result');
+const playerSelectedBtn = document.querySelector('.player-btn');
+const computerSelectedBtn = document.querySelector('.computer-btn');
+//
 
 window.onload = () => {
     if(localStorage.getItem("computerScore") && localStorage.getItem("playerScore")) {
         computer.innerHTML = localStorage.getItem("computerScore");
         player.innerHTML = localStorage.getItem("playerScore");
     }
+}
+
+const assignIconToPlayer = (playerBtn) => {
+    const img = document.createElement("img");
+    if(playerBtn) {
+        img.setAttribute('src', `./assets/${playerBtn.value}.png`);
+        img.setAttribute('alt', `${playerBtn.value}`);
+        playerBtn.appendChild(img);
+        playerBtn.classList.add(`${playerBtn.value}-btn`);
+    } else {
+        throw Error('Error while receiving value');
+    }
+}
+
+const assignIconToComputer = (computerBtn) => {
+    const img = document.createElement("img");
+    if(computerBtn) {
+        img.setAttribute('src', `./assets/${computerBtn.value}.png`);
+        img.setAttribute('alt', `${computerBtn.value}`);
+        computerBtn.appendChild(img);
+        computerBtn.classList.add(`${computerBtn.value}-btn`);
+    } else {
+        throw Error('Error while receiving value');
+    }
+}
+
+const removePrevValue = (playerBtn,computerBtn) => {
+    if(playerBtn.firstElementChild && computerBtn.firstElementChild) {
+        playerBtn.removeChild(playerBtn.firstElementChild);
+        computerBtn.removeChild(computerBtn.firstElementChild);
+        playerBtn.classList.remove(`${playerBtn.value}-btn`);
+        computerBtn.classList.remove(`${computerBtn.value}-btn`);
+    }
+}
+
+const oneRoundResult = (message,playerChoice,computerChoice) => {
+    const resultText = document.querySelector('.result-text');
+    playerSelectedBtn.value = playerChoice;
+    computerSelectedBtn.value = computerChoice;
+    assignIconToPlayer(playerSelectedBtn);
+    assignIconToComputer(computerSelectedBtn);
+    resultText.innerHTML = message;
+    playerButtons.classList.add('hide');
+    roundResult.classList.remove('hide');
+    const replay = document.querySelector('.replay');
+    replay.addEventListener('click',() => {
+        roundResult.classList.add('hide');
+        playerButtons.classList.remove('hide');
+        removePrevValue(playerSelectedBtn,computerSelectedBtn);
+    });
 }
 
 const resultTally = (playerScore,computerScore) => {
@@ -20,43 +78,85 @@ const resultTally = (playerScore,computerScore) => {
 const  result = (message) => {
     let playerScore = parseInt(player.innerHTML);
     let computerScore = parseInt(computer.innerHTML);
-    const nextButton = document.querySelector('.next');
     switch(message) {
         case "You Win":
             playerScore++;
-            nextButton.classList.add('show');
+            nextButton.classList.remove('hide');
             resultTally(playerScore,computerScore);
             break;
         case "You Lose":
             computerScore++;
+            nextButton.classList.add('hide');
             resultTally(playerScore,computerScore);
             break;
         case "Tie":
+            nextButton.classList.add('hide');
             resultTally(playerScore,computerScore);
             break;
     }
     
 }
 
+const toggleToHurray = (playingArea,victoryPage) => {
+    nextButton.classList.add('hide');
+    resetButton.classList.add('hide');
+    playingArea.classList.add('hide');
+    victoryPage.classList.remove('hide');
+}
+
+const toggleToPlayingArea = (playingArea,victoryPage) => {
+    resetButton.classList.remove('hide');
+    playerButtons.classList.remove('hide');
+    playingArea.classList.remove('hide');
+    roundResult.classList.add('hide');
+    victoryPage.classList.add('hide');
+}
+
+
+const showVictory = () => {
+    const playingArea = document.querySelector('.playing-area');
+    const victoryPage = document.querySelector('.victory');
+    const playAgain = document.querySelector('.play-again');
+    nextButton.addEventListener('click', () => toggleToHurray(playingArea,victoryPage));
+    playAgain.addEventListener('click',() => {
+        removePrevValue(playerSelectedBtn,computerSelectedBtn);
+        toggleToPlayingArea(playingArea,victoryPage)
+    });
+}
+
+showVictory();
+
 const reset = () => {
-    const resetBtn = document.getElementById("reset");
-    resetBtn.addEventListener('click', () => {
+    resetButton.addEventListener('click', () => {
         resultTally(0,0);
     }); 
 }
 
 reset();
 
+const toggleRuleBox = () => {
+    const rulesBox = document.querySelector('.rule-box');
+    const closeButton = document.querySelector('.cross'); 
+    const openButton = document.querySelector('.rules');
+    openButton.addEventListener('click', () => {
+        rulesBox.classList.remove('hide');
+    });
+    closeButton.addEventListener('click', () => {
+        rulesBox.classList.add('hide');
+    });
+}
 
-const winner = (p1,p2) => {
-    if(moves.includes(p1) && moves.includes(p2)) {
-        switch(p1) {
+toggleRuleBox();
+
+const winner = (player1,player2) => {
+    if(moves.includes(player1) && moves.includes(player2)) {
+        switch(player1) {
             case "Rock":
-                return p2 === "Rock" ? "Tie": p2 === "Paper" ? "You Lose": "You Win";
+                return player2 === "Rock" ? "Tie": player2 === "Paper" ? "You Lose": "You Win";
             case "Paper":
-                return p2 === "Rock" ? "You Win": p2 === "Paper" ? "Tie": "You Lose";
+                return player2 === "Rock" ? "You Win": player2 === "Paper" ? "Tie": "You Lose";
             case "Scissors":
-                return p2 === "Rock" ? "You Lose": p2 === "Paper" ? "You Win": "Tie";
+                return player2 === "Rock" ? "You Lose": player2 === "Paper" ? "You Win": "Tie";
         }
     } else {
         throw Error("Invalid Move");
@@ -71,11 +171,12 @@ function computerMove() {
 computerMove();
 
 function playerMove() {
-    let move
     const buttons = document.querySelectorAll('.play-btn');
     buttons.forEach(btn => btn.addEventListener('click',(e) => {
-        move = e.target.innerHTML;
-        const winnerMessage = winner(move,computerMove());
+        const myMove =  e.target.value || e.target.parentElement.value;
+        const pcMove = computerMove();
+        const winnerMessage = winner(myMove,pcMove);
+        oneRoundResult(winnerMessage,myMove,pcMove);
         result(winnerMessage);
     }));
 }
